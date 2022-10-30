1659db566ea96f8e2dcd3bcaeb2c74ad265951b7
Mariusea <marijusdilys9@gmail.com>
2022-10-30 10:56:54 +0200

    Bootstrap Added

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -23,11 +23,12 @@
     </div> --}}
 
     
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+<nav class=" bg-gradient-to-b from-red-600 border-gray-200 px-2 sm:px-4 py-1.5">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
       <a href="https://flowbite.com/" class="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
-          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          {{-- <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo"> --}}
+          <img src="https://part.lt/img/f1f179e4c4cfee5953af021a0003e4b6456.png" height="70px" width="70px" style="display: block" />
+          {{-- <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Happy Pizza</span> --}}
       </a>
       <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
