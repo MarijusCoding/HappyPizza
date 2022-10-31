f2eaf0fd94485d6f194a2f229f477d60a5700892
Mariusea <marijusdilys9@gmail.com>
2022-10-31 08:09:51 +0200

    PC navbar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,7 +7,21 @@
     <title>Happy Pizza 🍕</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
+<body class="m-0">
+    <div class="flex bg-gradient-to-b from-red-500 m-0 justify-evenly items-center border-2">
+      {{-- <div class="inline-block"> --}}
+        <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">MENIU</a>
+        <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">UŽSISAKYK</a>
+      {{-- </div> --}}
+      {{-- <div class="inline-block"> --}}
+        <img src="https://part.lt/img/f1f179e4c4cfee5953af021a0003e4b6965.png" class="sm:w-32 w-24"/>
+      {{-- </div> --}}
+      {{-- <div class="inline-block"> --}}
+        <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">KONTAKTAI</a>
+        <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">APIE</a>
+      {{-- </div> --}}
+      {{-- MOBILE VERSION --}}
+    </div>
     @yield('content')
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.app')
 
 @section('content')
-    <h1 style="text-align: center">q</h1>
+    
 @endsection
