d18bafafd3ce778239841806f4c69f2be189746b
Mariusea <marijusdilys9@gmail.com>
2022-11-01 10:25:17 +0200

    Navbar almost

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-// import './bootstrap.js';
+import './bootstrap.js';
+var opener = false;
+var togleris = document.getElementById("toggle");
+var tekstas = document.getElementById("teks");
+togleris.onclick = function () {
+    if (opener) {
+        togleris.src = "http://127.0.0.1:8000/toggle_icon.png";
+        console.log(togleris.src);
+        tekstas.style.visibility = "hidden";
+        opener = false;
+    } else {
+        togleris.style.width = "32px";
+        togleris.src = "http://127.0.0.1:8000/times_icon.png";
+        tekstas.style.visibility = "visible";
+        opener = true;      
+    }
+    
+}
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,24 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Happy Pizza 🍕</title>
+    <title>Happy Pizza</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="m-0">
-    <div class="flex bg-gradient-to-b from-red-500 m-0 justify-evenly items-center border-2">
+    <div class="flex bg-gradient-to-b from-red-500 m-0 sm:justify-evenly sm:pt-0 pt-1 sm:pl-0 pl-5 items-center border-2">
       {{-- <div class="inline-block"> --}}
         <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">MENIU</a>
         <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">UŽSISAKYK</a>
       {{-- </div> --}}
-      {{-- <div class="inline-block"> --}}
-        <img src="https://part.lt/img/f1f179e4c4cfee5953af021a0003e4b6965.png" class="sm:w-32 w-24"/>
-      {{-- </div> --}}
+      <div class=" sm:items-center items-start">
+        <a href="#"><img src="https://part.lt/img/f1f179e4c4cfee5953af021a0003e4b6965.png" class="sm:w-32 w-24"/></a>
+      </div>
       {{-- <div class="inline-block"> --}}
         <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">KONTAKTAI</a>
         <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">APIE</a>
       {{-- </div> --}}
       {{-- MOBILE VERSION --}}
+      <div class="sm:hidden visible w-full px-5 float-right">
+        <input type="image" src="{{ asset('toggle_icon.png') }}" id="toggle" class="sm:hidden block float-right">
+      </div>
     </div>
+    <h1 style="visibility:hidden" id="teks">LABAS</h1>
     @yield('content')
 </body>
 </html>
