db71f01efc99fb6847f675301de9800406d2e19f
Mariusea <marijusdilys9@gmail.com>
2022-11-03 13:58:10 +0200

    Updated

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,32 +8,32 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="m-0 p-0">
-    <div class="fixed top-0 w-full z-10 inline-flex bg-gradient-to-b border-0 from-red-500 to-white m-0 sm:justify-evenly sm:pt-0 pt-1 sm:pl-0 pl-5 items-center" id="navas">
+    <div class="fixed top-0 w-full z-10 inline-flex bg-gradient-to-b border-0 from-red-600  m-0 sm:justify-evenly sm:pt-0 pt-1 sm:pl-0 pl-5 items-center" id="navas">
       {{-- <div class="inline-block"> --}}
-        <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">MENIU</a>
-        <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">UŽSISAKYK</a>
+        <a href="#" class="text-2xl font-light no-underline text-white font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">MENIU</a>
+        <a href="#" class="text-2xl font-light no-underline text-white font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">UŽSISAKYK</a>
       {{-- </div> --}}
       <div class=" sm:items-center items-start">
         <a href="#"><img src="https://part.lt/img/f1f179e4c4cfee5953af021a0003e4b6965.png" class="sm:w-32 w-24"/></a>
       </div>
       {{-- <div class="inline-block"> --}}
-        <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">KONTAKTAI</a>
-        <a href="#" class="text-2xl font-light no-underline text-black font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">APIE</a>
+        <a href="#" class="text-2xl font-normal no-underline text-white font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">KONTAKTAI</a>
+        <a href="#" class="text-2xl font-normal no-underline text-white font-sans transition-all duration-150 hover:pb-2 hidden sm:flex">APIE</a>
       {{-- </div> --}}
       {{-- MOBILE VERSION --}}
       <div class="sm:hidden visible w-full px-5 float-right">
         <input type="image" src="{{ asset('toggle_icon.png') }}" id="toggle" class="sm:hidden block float-right">
       </div>
     </div>
-    <div class="fixed top-20 text-center items-center justify-center mx-auto w-full bg-white" style="display: none; z-index: 900;" id="teks">
-      <ul class="list-none px-0">
+    <div class="fixed text-center items-center justify-center mx-auto w-full bg-white" style="display: none; z-index: 900; top: 4.9rem;" id="teks">
+      <ul class="list-none py-2">
         <li><a href="#" class="text-2xl text-black font-sans no-underline" id="hambur">MENIU</a></li><hr class="w-24">
         <li><a href="#" class="text-2xl text-black font-sans no-underline" id="hambur">UŽSISAKYK</a></li><hr class="w-24">
         <li><a href="#" class="text-2xl text-black font-sans no-underline" id="hambur">KONTAKTAI</a></li><hr class="w-24">
         <li><a href="#" class="text-2xl text-black font-sans no-underline" id="hambur">APIE</a></li>
       </ul>
     </div>
-    <div class="flex flex-wrap justify-around pt-44">
+    <div class="flex flex-wrap justify-around sm:pt-32 pt-20">
       @yield('content')
     </div>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,52 +1,12 @@
 @extends('layout.app')
 
 @section('content')
+        @for ($i = 0; $i < 15; $i++)
         <div class="relative inline-block max-w-md min-w-min p-3" style="">
             <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
+            <h2>Titleas</h2>
+            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full actove:bg-red-800">Išsamiau</a>
         </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
-        <div class="relative inline-block max-w-md min-w-min p-3" style="">
-            <img src="{{ asset("picos/1.jpg") }}" class="w-full brightness-50 hover:brightness-75 transition-all duration-300" style="filter: brightness(50%)" alt="">
-            <a href="#" class=" absolute font-sans no-underline border-3 border-red-700 bg-red-600 hover:bg-red-700 py-3 px-5 bottom-6 right-6 text-white rounded-full">Išsamiau</a>
-        </div>
+        @endfor
+        
 @endsection
